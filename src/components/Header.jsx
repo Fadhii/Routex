60666a436a92da9ad3504e4706765a226f560be8
unknown <fadhilhussain12@gmail.com>
2024-09-02 19:25:34 +0530

    final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -86,7 +86,7 @@ function Header() {
             </a>
           </li>
           <li>
-    <div className="lg:hidden self-center flex items-center space-x-2 bg-gray-100 mr-4 rounded-md">
+    <div className="lg:hidden self-center flex items-center space-x-2 mr-4 rounded-md">
         <i className="fa-regular fa-comments text-green-950 text-2xl"></i>
         <div>
             <p className="text-xs font-thin text-black">Need help?</p>
@@ -97,7 +97,7 @@ function Header() {
 
         </ul>
        
-        <div className="hidden lg:flex items-center space-x-2 bg-gray-100 mr-4 rounded-md">
+        <div className="hidden lg:flex items-center space-x-2 mr-4 rounded-md">
     <i className="fa-regular fa-comments text-green-950 text-2xl"></i>
     <div>
         <p className="text-xs font-thin text-black">Need help?</p>
